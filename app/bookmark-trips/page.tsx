81ad6d7a78dc9e4c83ae0d7b74038fc27052aeaf
shristi-gupta21 <gshristi@gmail.com>
2026-07-06 17:43:35 +0530

    header ui & logo added

---
+++ app/bookmark-trips/page.tsx
@@ -0,0 +1,13 @@
+import { type Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Bookmarked trips",
+  description: "Segregate your favourite trips",
+};
+
+const BookmarkTrips = () => {
+  return <div>BookmarkTrips</div>;
+};
+
+export default BookmarkTrips;
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,7 +1,24 @@
+import { Bookmark } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+import { Input } from "../ui/input";
+
 const header = () => {
-  return <div>header</div>;
+  return (
+    <div className="flex justify-between">
+      <Link href={"/discover"}>
+        <Image src="/livetrip_icon.svg" alt="LiveTrip" width={24} height={24} />
+      </Link>{" "}
+      <div className="w-1/3">
+        <Input placeholder="Search place" />
+      </div>
+      <Link href={"/bookmark-trips"}>
+        <Bookmark />
+      </Link>
+    </div>
+  );
 };
 
 export default header;
