f9f7eea60f5065de2aa7545d2a98d42b518ae782
shristi-gupta21 <gshristi@gmail.com>
2026-07-11 09:45:24 +0530

    delay function in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const delay = (ms: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(resolve, ms);
+  });
